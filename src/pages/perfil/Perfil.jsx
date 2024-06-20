6fc17f94452ce2e2ad016bd7f5ea880f234905b6
YagoTeles <yago.teles@hotmail.com>
2024-06-19 23:03:40 -0300

    perfil left update

--- src/pages/perfil/Perfil.jsx
+++ src/pages/perfil/Perfil.jsx
@@ -1,25 +1,50 @@
 import "./perfil.css"
 import Card from "../../components/card/Card";
 import dataDesc from "../../data/dataDesc";
-import ProfileComponent from "./profileComponent";
+import ProfileCards from "./ProfileCard";
 
 function Perfil() {
     const capacidadesCards = dataDesc.filter(item => item.type === 'C');
     const resultadosCards = dataDesc.filter(item => item.type === 'R');
+    const indicesCards = dataDesc.filter(item => item.type === 'I');
+    const cardStyle = {
+        backgroundColor: '#FFFFFF',
+        borderRadius: '10px',
+        boxShadow: '0 4px 15px rgba(0, 0, 0, 0.3)',
+        boxSizing: 'border-box',
+        height: 100,
+        width: 170,
+        display: 'flex',
+        alignItems:'end'
+    }
     return (  
         <div style={{display: 'flex',padding: '20px'}}>
-            <div className="separator">             
-                <ProfileComponent color={'#2F80ED'} title={'Capacidades'} dataCards={capacidadesCards}/>
-                <br/>
-                <ProfileComponent color={'#3BC3DF'} title={'Resultados'} dataCards={resultadosCards}/> 
+            <div className="separator">   
+                <ProfileCards color={'#2F80ED'} title={'Capacidades'} dataCards={capacidadesCards} cardStyle={cardStyle}/>
+                <br/> 
+                <ProfileCards color={'#3BC3DF'} title={'Resultados'} dataCards={resultadosCards} cardStyle={cardStyle}/> 
             </div>
             <div className="separator">
                 <Card
-                width={600}
+                width={650}
                 height={650}
                 >
-                    <div>
-                        
+                    <div className="box-regiao">
+                        <div style={{color:'#1A428A',fontWeight:'bold',fontSize:'21px'}}>
+                            Nordeste
+                        </div>
+                        <div className="region-summary"> 
+                            {indicesCards.map((item, index) => (
+                                <Card cardStyle = {cardStyle}>
+                                    <div className="profile-down-card" style={{backgroundColor:'#2F80ED'}}>
+                                        {item.label}
+                                    </div>
+                                </Card>                    
+                            ))}                    
+                        </div>
+                        <div style={{color:'#1A428A',fontWeight:'bold',fontSize:'21px'}}>
+                            Desempenho Indicadores
+                        </div>
                     </div>
                 </Card>
             </div>
--- src/pages/perfil/ProfileCard.jsx
+++ src/pages/perfil/ProfileCard.jsx
@@ -2,36 +2,25 @@ import "./perfil.css"
 import Card from "../../components/card/Card";
 import dataDesc from "../../data/dataDesc";
 import './perfil.css'
-function profileComponent({children,color,title,dataCards}) {
+function ProfileCards({color,dataCards,title,cardStyle}) {
     
-    const cardStyle = {
-        backgroundColor: '#FFFFFF',
-        borderRadius: '10px',
-        boxShadow: '0 4px 15px rgba(0, 0, 0, 0.3)',
-        boxSizing: 'border-box',
-        height: 100,
-        width: 170,
-        display: 'flex',
-        alignItems:'end'
-    }
     return (  
     <>
     <div className='title-base-profile' style={{backgroundColor: color}}>
         {title}
-    </div>
-    <div className="profile-cards">   
+    </div> 
+    <div className="profile-cards"> 
     {dataCards.map((item, index) => (
         <Card cardStyle = {cardStyle}>
             <div className="profile-down-card" style={{backgroundColor:color}}>
                 {item.label}
             </div>
         </Card>                    
-        ))}
-        
+        ))} 
     </div>
     </>
 
     );
 }
 
-export default profileComponent;
+export default ProfileCards;
